b725f90b7c757e1583fdc2b3fff3eaa0ff84bad6
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-10-12 17:55:46 -0300

    Implementação do client FindAddressByZipCodeClient

--- src/main/java/br/com/everton/arquiteturahexagonaljava/ArquiteturaHexagonalJavaApplication.java
+++ src/main/java/br/com/everton/arquiteturahexagonaljava/ArquiteturaHexagonalJavaApplication.java
@@ -2,7 +2,9 @@ package br.com.everton.arquiteturahexagonaljava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ArquiteturaHexagonalJavaApplication {
 
