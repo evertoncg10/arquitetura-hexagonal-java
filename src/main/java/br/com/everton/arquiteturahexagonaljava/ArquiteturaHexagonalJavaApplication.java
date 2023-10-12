e4c18300686d934a9ac5e90918a6c74506b5402b
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-10-12 15:21:09 -0300

    Criando o projeto e inserindo as dependências.

---
+++ src/main/java/br/com/everton/arquiteturahexagonaljava/ArquiteturaHexagonalJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.everton.arquiteturahexagonaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaHexagonalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaHexagonalJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/everton/arquiteturahexagonaljava/ArquiteturaHexagonalJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.everton.arquiteturahexagonaljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaHexagonalJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
