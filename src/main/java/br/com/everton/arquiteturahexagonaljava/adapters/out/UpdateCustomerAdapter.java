b6fce1394571e177e03610a3ec5260bc072dc2ce
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-10-22 13:35:41 -0300

    Implementação do adapter do UpdateCustomer.

---
+++ src/main/java/br/com/everton/arquiteturahexagonaljava/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,25 @@
+package br.com.everton.arquiteturahexagonaljava.adapters.out;
+
+import br.com.everton.arquiteturahexagonaljava.adapters.in.controller.mapper.CustomerMapper;
+import br.com.everton.arquiteturahexagonaljava.adapters.out.repository.CustomerRepository;
+import br.com.everton.arquiteturahexagonaljava.adapters.out.repository.mapper.CustomerEntityMapper;
+import br.com.everton.arquiteturahexagonaljava.application.core.domain.Customer;
+import br.com.everton.arquiteturahexagonaljava.application.ports.out.UpdateCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
